12e2a43df73a9ccd041df71a41c89866bfbbf6be
optimusdrift <optimusdrift@gmail.com>
2019-10-03 20:14:53 -0300

    nueva ventana de login

--- application/views/prototipo/login.php
+++ application/views/prototipo/login.php
@@ -1,36 +1,72 @@
 <body background="<?php echo base_url()."assets/"; ?>fondoLogin.jpg">
-<br>
-<br>
-<br>
-<br>
-<!-- Horizontal Form -->
-<div class="row justify-content-center">
-    <div class="card-dark card col-4">
-        <div class="card-header">
-        <h3 class="card-title">Iniciar Sesión</h3>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url().'assets/'; ?>plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo base_url().'assets/'; ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url().'assets/'; ?>dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="<?php echo base_url().'index.php/prototipo'; ?>"><b>Wooden</b>House</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Inicia sesión para comenzar.</p>
+
+      <form action="<?php echo base_url().'index.php/prototipo';?>" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Correo electrónico">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Contraseña">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block btn-outline-secondary btn-flat badge-dark">Ingresar</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
     </div>
-    <!-- /.card-header -->
-    <!-- form start -->
-    <form class="form-horizontal">
-        <div class="card-body">
-            <div class="form-group row">
-            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-            </div>
-            </div>
-            <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 control-label">Contraseña</label>
-            <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña">
-            </div>
-            </div>
-        </div>
-        </form> <!--Mover el /form abajo del /div ante ultimo -->
-        <!-- /.card-body -->
-        <div class="card-footer">
-           <!--type="submit" quitar <a> y agregar el type-->
-           <a href="<?php echo base_url().'index.php/prototipo';?>"><button class="btn btn-dark">Iniciar Sesión</button></a> 
-        </div>
-    <!-- /.card-footer -->
+    <!-- /.login-card-body -->
+  </div>
 </div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?php echo base_url().'assets/'; ?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url().'assets/'; ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
